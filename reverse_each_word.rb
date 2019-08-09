7258907d861411fe4104496017d4bf05bd697bd8
Joshua Hill <josh@jlhworkshop.com>
2019-08-09 15:28:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(args)
   arry = args.split(" ")
   # puts arry[0].reverse
   newArry = []
-  arry.each do |index|
-    newArry.push(arry[index].reverse)
+  arry.each do |word|
+    newArry.push(arry[word].reverse)
   end
   puts newArry
 end
