1aa0775533ec01ad1dd3f8b3bcf0555b83d5fcbf
Joshua Hill <josh@jlhworkshop.com>
2019-08-09 15:25:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 def reverse_each_word(args)
   arry = args.split(" ")
+  puts arry[0].reverse
   newArry = []
-  arry.each do |index|
-    newArry.push = arry[index].reverse
-  end
-  puts newArry
+  # arry.each do |index|
+  #   newArry.push = arry[index].reverse
+  # end
+  # puts newArry
 end
 
 reverse_each_word("Hello there, and how are you?")
